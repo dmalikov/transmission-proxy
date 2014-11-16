682e5b8ed2bb0599d951604d6f4038af94cbf8dc
Dmitry Malikov <malikov.d.y@gmail.com>
2014-11-16 22:38:17 +0300

    Move processed torrents to a related directory

--- Main.hs
+++ Main.hs
@@ -1,40 +1,61 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
 import           Control.Applicative
+import           Control.Exception
 import           Control.Lens
+import           Control.Monad
 import           Data.BEncode
 import qualified Data.ByteString                 as BS
 import qualified Data.ByteString.Char8           as BSC
 import           Network.HsTorrent.TorrentParser
 import           Network.URI
+import           System.Directory
 import           System.Environment
+import           System.FilePath
+import           System.FilePath.Find
+import           System.FilePath.Posix
 import           System.Process
 
+download_dir = "/volume1/homes/transmission/"
+host = "192.168.1.43"
+
+base_dir = "/home/m/downloads/torrents"
+new_dir = base_dir </> "new"
+failed_dir = base_dir </> "failed"
+done_dir = base_dir </> "done"
 
 main :: IO ()
 main = do
-  torrentFile <- parseArgs
+  torrents <- find always (fileType ==? RegularFile &&? extension ==? ".torrent") new_dir
+  forM_ torrents $ \torrent -> do
+    BSC.putStr $ BSC.pack $ "processing " ++ torrent ++ " :"
+    result <- process torrent
+    BSC.putStrLn $ BSC.pack $ show result
+    case result of
+      True   -> moveTo done_dir torrent
+      False  -> moveTo failed_dir torrent
+ where
+  moveTo :: FilePath -> FilePath -> IO ()
+  moveTo dir torrent = renameFile torrent (dir </> (takeFileName torrent))
+
+process :: FilePath -> IO Bool
+process torrentFile = do
   torrent <- BS.readFile torrentFile
   case view tAnnounce <$> decode torrent of
-    Left e -> error e
+    Left e -> do
+      BSC.putStr $ BSC.pack e
+      return False
     Right uri -> do
-      BSC.putStrLn ("Torrent announce: " `BSC.append` uri)
       case trackerSuffix =<< uriRegName <$> (uriAuthority =<< parseURI (BSC.unpack uri)) of
         Just s -> do
-          callProcess "transmission-remote"
-            [ "192.168.1.33"
-            , "--add",  torrentFile
-            , "--download-dir", "/mnt/wdt/" ++ s
-            ]
-        Nothing -> putStrLn "unknown tracker"
-
-
-parseArgs :: IO FilePath
-parseArgs = do
-  args <- getArgs
-  case args of
-    (x:_) -> return x
-    _ -> error "undefined torrent file"
+          handle (\(SomeException _) -> return False) $ do
+            callProcess "transmission-remote"
+              [ host
+              , "--add",  torrentFile
+              , "--download-dir", download_dir ++ s
+              ]
+            return True
+        Nothing -> return True
 
 trackerSuffix :: String -> Maybe String
 trackerSuffix s = case s of
