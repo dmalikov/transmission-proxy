324634d51f1d75ac32d442e24b484e5b21bede39
Dmitry Malikov <malikov.d.y@gmail.com>
2015-01-06 00:45:07 +0300

    Update to latest nix

--- hstorrent.nix
+++ hstorrent.nix
@@ -6,7 +6,7 @@ haskellPackages.cabal.mkDerivation (self: {
   pname = "hstorrent";
   version = "0.1.0.0";
   src = pkgs.fetchgit {
-    url = "git@github.com:hstorrent/hstorrent.git";
+    url = "https://github.com/hstorrent/hstorrent";
     sha256 = "a35099d1d65007d1b97fac2353aac876ed638a4c6d0aaeed1ecff8b2762426ee";
     rev = "3a8887aaeb7e6239685c0ad5fcf370156c840d25";
     fetchSubmodules = false;
